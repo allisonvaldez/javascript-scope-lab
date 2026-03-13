72eb13c1e61a18085eb7b62fc4b7bc2e4ea7e13d
allisonvaldez <alleyvaldez1@outlook.com>
2026-03-13 04:04:19 -0700

    done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
   // Declare a variable in the global scope called burgers assign it the value of an array for 2 elements 'Hamburger' and 'Cheeseburger'.
-let burgers = ["Hamburger", "Cheeseburger"];
+const burgers = ["Hamburger", "Cheeseburger"];
 
 // Declare a variable in the global scope called featuredDrink and assign it the value 'Strawberry Milkshake'.
 let featuredDrink= "Strawberry Milkshake";
@@ -8,23 +8,19 @@ let featuredDrink= "Strawberry Milkshake";
     1. Creates a function-scoped variable named newBurger and assigns it the value 'Flatburger'.
     2. Uses the .push() array method to add newBurger to the burgers array.
 */
-function addBurger  (){
-    let newBurger = "Flatburger'";
+function addBurger() {
+    const newBurger = "Flatburger";
     burgers.push(newBurger);
-
-    // Write an if statement whose condition is true, so that the code in its block will always execute.
-    if(true){
-        let anotherNewBurger = "Maple Bacon Burger";
-        burgers.push(anotherNewBurger);
-    }
 };
+// Write an if statement whose condition is true, so that the code in its block will always execute.
+if(true) {
+    const anotherNewBurger = "Maple Bacon Burger";
+    burgers.push(anotherNewBurger);
+}
 
 /*Write a function named changeFeaturedDrink that, when called:
     1. changes the value of the FeaturedDrink variable to 'The JavaShake'.
 */
 function changeFeaturedDrink(){
-    let featuredDrink = "The JavaShake";
+    featuredDrink = "The JavaShake";
 };
-
-addBurger();
-changeFeaturedDrink();
